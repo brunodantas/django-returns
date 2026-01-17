da4347bfd9e193b6ecf95e5d6d15a34a5f943d5d
brunodantas <bruno.welldm@gmail.com>
2026-01-16 21:22:59 -0300

    annotate

--- src/django_returns/models.pyi
+++ src/django_returns/models.pyi
@@ -1,8 +1,12 @@
 from django.db import models
 from returns.result import Result
 
+from django_returns.managers import ReturnsManager
+
 class ReturnsModel(models.Model):
     """Model base class that provides safe methods via returns."""
+
+    objects: ReturnsManager
 
     def save_safe(self, *args, **kwargs) -> Result: ...
     def delete_safe(self, *args, **kwargs) -> Result: ...
