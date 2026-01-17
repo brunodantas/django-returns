2d634bc197518be39d970d374a5dac0a0712273c
brunodantas <bruno.welldm@gmail.com>
2026-01-17 00:11:37 -0300

    add utils

--- src/django_returns/utils.py
+++ src/django_returns/utils.py
@@ -1,7 +1,13 @@
 from returns.result import Result, safe
+from returns.unsafe import unsafe_perform_io as _unsafe_perform_io
 
 
 @safe
 def getattr_safe(obj, name) -> Result:
     """Safe version of getattr that returns a Result."""
     return getattr(obj, name)
+
+
+def unsafe_perform_io(io):
+    """Unwraps IOResult."""
+    return _unsafe_perform_io(io)
