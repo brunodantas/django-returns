657a23fec6414148750353d3443ee5b9c3203fe0
brunodantas <bruno.welldm@gmail.com>
2026-01-22 21:13:29 -0300

    refactor: update typing for clarity

--- src/django_returns/models.pyi
+++ src/django_returns/models.pyi
@@ -1,20 +1,26 @@
-from typing import TypeVar
+from typing import TypeAlias, TypeVar
 
 from django.db import models
-from returns.result import Result
+from returns.result import Failure, Success
 
 from django_returns.managers import ReturnsManager
 
 _M = TypeVar("_M", bound="ReturnsModel")
+_Value = TypeVar("_Value")
+_Error = TypeVar("_Error", bound=Exception)
+
+MatchResult: TypeAlias = Success[_Value] | Failure[_Error]
 
 class ReturnsModel(models.Model):
     """Model base class that provides safe methods via returns."""
 
     objects: ReturnsManager
 
-    def save_result(self, *args, **kwargs) -> Result[None, Exception]: ...
+    def save_result(self, *args, **kwargs) -> MatchResult[None, Exception]: ...
     def delete_result(
         self, *args, **kwargs
-    ) -> Result[tuple[int, dict[str, int]], Exception]: ...
-    def full_clean_result(self, *args, **kwargs) -> Result[None, Exception]: ...
-    def refresh_from_db_result(self, *args, **kwargs) -> Result[None, Exception]: ...
+    ) -> MatchResult[tuple[int, dict[str, int]], Exception]: ...
+    def full_clean_result(self, *args, **kwargs) -> MatchResult[None, Exception]: ...
+    def refresh_from_db_result(
+        self, *args, **kwargs
+    ) -> MatchResult[None, Exception]: ...
